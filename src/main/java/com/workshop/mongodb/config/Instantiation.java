64ee46354d1a9a5c39673797ad80006ff9f5a20d
Alison-Vizoni <alison.vizoni@gmail.com>
2021-12-20 14:04:55 -0300

    Arquivo de instanciacao da base de dados

---
+++ src/main/java/com/workshop/mongodb/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.workshop.mongodb.config;
+
+import com.workshop.mongodb.Repository.UserRepository;
+import com.workshop.mongodb.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User alex = new User("Alex", "alex@example");
+        User mario = new User("Mario", "mario@example");
+        User ana = new User("Ana", "ana@example");
+
+        userRepository.saveAll(Arrays.asList(alex, mario, ana));
+    }
+}
--- src/main/java/com/workshop/mongodb/domain/User.java
+++ src/main/java/com/workshop/mongodb/domain/User.java
@@ -18,6 +18,11 @@ public class User implements Serializable {
     public User() {
     }
 
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
     public User(String id, String name, String email) {
         this.id = id;
         this.name = name;
