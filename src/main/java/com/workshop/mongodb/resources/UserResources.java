597dd37015f0e50a8495fe7ddf079d5f1509248a
Alison-Vizoni <alison.vizoni@gmail.com>
2021-12-20 14:11:35 -0300

    Criando userDTO

--- src/main/java/com/workshop/mongodb/resources/UserResources.java
+++ src/main/java/com/workshop/mongodb/resources/UserResources.java
@@ -2,6 +2,7 @@ package com.workshop.mongodb.resources;
 
 import com.workshop.mongodb.Service.UserService;
 import com.workshop.mongodb.domain.User;
+import com.workshop.mongodb.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,8 +20,9 @@ public class UserResources {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> users = userService.findAll();
-        return ResponseEntity.ok().body(users);
+        List<UserDTO> usersDTO = users.stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(usersDTO);
     }
 }
