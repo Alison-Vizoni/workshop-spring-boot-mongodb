b87d2b75ea57a1ae95ab8851b1d45e127defc972
Alison-Vizoni <alison.vizoni@gmail.com>
2021-12-20 11:15:02 -0300

    Teste de conexao com mongodb ao procurar todos os usuarios

---
+++ src/main/java/com/workshop/mongodb/resources/UserResources.java
@@ -0,0 +1,25 @@
+package com.workshop.mongodb.resources;
+
+import com.workshop.mongodb.Service.UserService;
+import com.workshop.mongodb.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+}
